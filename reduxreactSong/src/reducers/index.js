229fb68afa8bc1d4c04c0d7bcd99b4beca7453c5
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2019-01-07 22:29:37 -0500

    learning the basic of redux

---
+++ reduxreactSong/src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+
+const songReducer = () => {
+	return [
+		{ title: 'No Scrubs', duration: '4:05' },
+		{ title: 'Macarena', duration: '2:30' },
+		{ title: 'All Star', duration: '3:45' },
+		{ title: 'I want it that whay', duration: '4:24' }
+
+
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if(action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+	return selectedSong;
+};	
+
+export default combineReducers ({
+	songs: songReducer,
+	selectedSongKey: selectedSongReducer
+});
